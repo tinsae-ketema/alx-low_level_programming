78e5c0b69ba500a27a780f785b16f8bc1dc40cf3
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-16 16:54:32 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,6 +26,11 @@ int main(void)
 	else if (lastd < 6 && lastd != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+	
+	}
+	else if (n == 98)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastd);
 	}
 	return (0);
 }
