d4d649e88f5683ddda2dc18488f3a8b89b502c6d
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-16 14:57:34 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,15 +7,15 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 0; n < '9'; n++)
+	for (n = '0'; n < '9'; n++)
 	{
-		for (m = n+1 ; m <= '9'; m++)
+		for (m = n + 1; m <= '9'; m++)
 		{
 			if (m != n)
 			{
 				putchar(n);
 				putchar(m);
-				if (n == '8' && m == 9)
+				if (n == '8' && m == '9')
 					continue;
 				putchar(',');
 				putchar(' ');
