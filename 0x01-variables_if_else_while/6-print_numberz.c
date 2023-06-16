6e4abf7306fbdc161c42f377e870f6cd015ab5ea
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-16 16:15:23 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,14 +3,14 @@
  * main - prints number from 0 to 9.
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int i;
 
 	for (i = '0'; i <= '9'; i++)
-	
-		putchar(i);
-		putchar('\n');
+	putchar(i);
+	putchar('\n');
 
-		return (0);
+	return (0);
 }
