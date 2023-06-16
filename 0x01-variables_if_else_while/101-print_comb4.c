db93baa9b81d30caf339fff34b1bc8d6a51b4b02
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-16 15:31:55 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,23 +9,22 @@ int main(void)
 
 	for (n = '0'; n < '9'; n++)
 	{
-		for (m = n + 1; m <= '9'; m++)
-		{
-			for (l = m + 1; l <= '9'; l++)
-			{
-				if ((m != n) != l)
-					{
-						putchar(n);
-						putchar(m);
-						putchar(l);
-						if (n == '7' && m == '8')
-							continue;
-						putchar(',');
-						putchar(' ');
-					
-					}
-			}
-		}
+	for (m = n + 1; m <= '9'; m++)
+	{
+	for (l = m + 1; l <= '9'; l++)
+	{
+	if ((m != n) != l)
+	{
+	putchar(n);
+	putchar(m);
+	putchar(l);
+	if (n == '7' && m == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
 	}
 	putchar('\n');
 	return (0);
