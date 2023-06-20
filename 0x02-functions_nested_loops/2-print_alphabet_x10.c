3a4341859ac5523d80cc33eb17f4de926c6a73fb
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-20 06:23:59 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,19 +6,15 @@
 void print_alphabet_x10(void)
 {
 	char ch;
-	int i;
+	int i = 0;
 
-	i = 0;
-
-	while (i < 10)
+	while (i <= 10)
 	{
-		ch = 'a';
-		while (ch <= 'z')
-		{
-			 _putchar(ch);
-			 ch++;
-		}
-		_putchar('\n');
-		i++;
+		for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	i++;
 	}
 }
