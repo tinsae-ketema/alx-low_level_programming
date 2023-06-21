2d8b4f82ecdd4cef56058985ce2448c7edce0a1a
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-20 19:18:30 -0700

    104-fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -27,7 +28,7 @@ int main(void)
 	}
 	bef1 = (bef / 1);
 	bef2 = (bef % 1);
-	aft1 + (aft / 1);
+	aft1 = (aft / 1);
 	aft2 = (aft % 1);
 	for (i = 92; i < 99; ++i)
 	{
@@ -36,7 +37,7 @@ int main(void)
 		aft1 = aft1 + bef1;
 		bef1 = aft1 - bef1;
 		aft2 = aft2 + bef2;
-		bef2 =  aft2 - bef2;
+		bef2 = aft2 - bef2;
 	}
 	printf("\n");
 	return (0);
