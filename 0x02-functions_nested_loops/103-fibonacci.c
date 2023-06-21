af4b520fa94991a8502fa3faf041c04a3dee62b0
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-20 18:08:29 -0700

    103-fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,14 +10,14 @@ int main(void)
 
 {
 	int i = 0;
-	int j = i, k = 2, sum = k;
+	int j = 1, k = 2, sum = k;
 
 	while (k + j < 400000)
 	{
 		k += j;
 		if (k % 2 == 0)
 			sum += k;
-		j = k + j;
+		j = k - j;
 		++i;
 	}
 	printf("%d\n", sum);
