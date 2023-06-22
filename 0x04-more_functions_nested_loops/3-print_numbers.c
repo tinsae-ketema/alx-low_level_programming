e74ee022fee31afda49625e6a523788b3174889f
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-22 13:42:59 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers since 0 up to 9
+ * Return: The numbers since 0 up to 9
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 0; c++)
+	{
+		_putchar(c + '0');
+	}
+	putchar('\n');
+}
