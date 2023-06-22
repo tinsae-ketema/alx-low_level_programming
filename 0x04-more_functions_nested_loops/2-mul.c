3f41e547e7e1d07c72d6c75d8c1074a2e857ff05
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-22 13:31:07 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two intigers
+ * @a: The first number to multiply
+ * @b: The second number to mutiply
+ * Return: The result of multiplication between a and b
+ */
+
+int mul(int a, int b)
+
+{
+	return ( a * b);
+}
