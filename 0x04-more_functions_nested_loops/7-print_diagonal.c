8082cc527b9acba0f75619d09fe951201adb59e9
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-22 15:01:49 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * pint_diagonal - draws diagonal line on the terminal
+ * print_diagonal - draws diagonal line on the terminal
  * @n: number of times the character \ should be printed
  */
 
