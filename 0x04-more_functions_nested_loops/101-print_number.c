fcb387354725e968c91bf9ee4a784d8bc975f814
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-22 19:38:38 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,10 +10,10 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('_');
+		_putchar('-');
 		num = -num;
 	}
 	if ((num / 10) > 0)
 		print_number(num / 10);
-	_putchar((num % 10) + 48);
+	_putchar((num % 10) + '0');
 }
