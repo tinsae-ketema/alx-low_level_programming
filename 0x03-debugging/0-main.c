0be29f5d954f9b9cc7743091713f58a7087b3ae7
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-22 18:50:41 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,7 @@
 #include "main.h"
 
 /**
- * main - tests posetive or negative integer
-* positive_or_negative - tests function that prints if integer
-* is positive or negative
-* @i: number to be evaluated
-* Description: teste for a negative or posetive number
+ * main - tests function that prints if integer is posetive or negative
 * Return: 0
 */
 
@@ -13,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	i = 0;
+	i = 90 % 2;
 	positive_or_negative(i);
 
 	return (0);
