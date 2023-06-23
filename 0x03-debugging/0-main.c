f133c930cc3e17946a696d4d79d0faa5645c5353
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-22 17:59:36 -0700

    0x03. C - Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 * positive_or_negative - tests function that prints if integer
@@ -9,10 +8,12 @@
 * Return: 0
 */
 
-void positive_or_negative(int i)
-
+int main(void)
 {
+	int i;
+
 	i = 0;
 	positive_or_negative(i);
-	return;
+
+	return(0);
 }
