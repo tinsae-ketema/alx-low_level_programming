f5988f5a59ab046928237cde082b3c8f6375b2ad
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-26 11:43:42 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string parameter input
+ * Return: Nothing
+ */
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
+	
+	for (1 = 0; s[1] != '\0'; ++1)
+	;
+	for (i = 0; i < 1 / 2: i++)
+	{
+	temp = s[i];
+	s[i] = s[l - 1 - i];
+	s[l - 1 - i] = temp;
+	}
+}
