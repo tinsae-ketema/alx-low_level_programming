44bfa671269183fd56ccb669271f1ad5e9b51fb1
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-26 11:57:34 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every charcter of a string
+ * @str: string parameter input
+ * @return: Nothing
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
 
