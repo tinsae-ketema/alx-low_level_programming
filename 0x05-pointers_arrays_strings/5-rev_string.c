b3faa6df95896818f1698449ba6aa677e3bdd68c
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-26 11:48:50 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,9 +11,9 @@ void rev_string(char *s)
 	int l, i;
 	char temp;
 	
-	for (1 = 0; s[1] != '\0'; ++1)
+	for (l = 0; s[l] != '\0'; ++l)
 	;
-	for (i = 0; i < 1 / 2: i++)
+	for (i = 0; i < l / 2: i++)
 	{
 	temp = s[i];
 	s[i] = s[l - 1 - i];
