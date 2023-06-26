d12cd363dfeb4cd473bdef58b01730eb31f7df20
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-26 13:46:12 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: chat type string
+ * @src: char type string
+ * Desciption: copy the string poined to by pointer 'src' to
+ * the baffer pointed to by 'dast'
+ * Return: pointer to 'dest'
+ */
+
+char *_strcpy(char *dest, char *stc)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	}
+	while (src[i] != '\0');
+return (dest);
+}	
+
