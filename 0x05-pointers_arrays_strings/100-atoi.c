0b528d75e6b06aa9ae386bbbdd007a1f8892a797
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-26 14:32:56 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -19,13 +19,13 @@ int _atoi(char *s)
 
 	while (i < len && f == 0)
 	{
-	if (s[i] == '-' )
+	if (s[i] =='-')
 		++d;
 	if (s[i] >= '0' && s[i] <= '9')
 	{
 		digit = s[i] - '0';
 		if (d % 2)
-			digit = - digit;
+			digit = -digit;
 		n = n * 10 + digit;
 		f = 1;
 		if (s[i + 1] < '0' || s[i + 1] > '9')
