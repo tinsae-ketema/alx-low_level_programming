43938022fd6bc5c2c1d6d683728e9c27d4ff999e
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-26 10:55:34 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,10 +3,10 @@
 
 #include <stdio.h>
 
-int _putchar(char c);
 void reset_to_98(int *n);
 int _strlen(char *s);
 int _atoi(char *S);
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void _puts(char *str);
 void print_rev(char *s);
