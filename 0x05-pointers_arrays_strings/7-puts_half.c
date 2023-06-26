81eee4d04a902360afb6bb768c707d9fe4d2162d
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-26 12:05:39 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "min.h"
+
+/**
+ * puts_half - print second half of a string
+ * @str: char stray string type
+ * Description: if odd number of chars, print (length - 1) / 2
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; st[i] != '\0'; i++)
+		;
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\0');
+}
