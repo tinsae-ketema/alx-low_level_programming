0b2848c650e0bd65ada840ffa2b3a4135fa78763
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-29 14:23:38 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,14 +7,14 @@
  * Return: 1 if true, 0 if false
  */
 
-int islower(char c)
+int isLower(char c)
 {
-	return (c >= 97 && c <== 122);
+	return (c >= 97 && c <= 122);
 }
 int isDelimiter(char c)
 {
 	int i;
-	char delimiter[] = " \t\n; . !?\*()()*;
+	char delimiter[] = " \t\n; . !?\"()()";
 
 	for (i = 0; i < 12; i++)
 		if (c == delimiter[i])
