0db5b0ce1891a3ace3e754863cf903c5d823a141
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-29 14:36:37 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * islower - determines whether ascii is lowercse
+ * isLower - determines whether ascii is lowercse
  * @c: character
  * Return: 1 if true, 0 if false
  */
@@ -11,6 +11,12 @@ int isLower(char c)
 {
 	return (c >= 97 && c <= 122);
 }
+
+/**
+ * isDelimiter - determines whether asci is a delimiter
+ * @c: character
+ * Return: 1 if true, 0 if false
+ */
 int isDelimiter(char c)
 {
 	int i;
@@ -21,6 +27,12 @@ int isDelimiter(char c)
 			return (1);
 	return (0);
 }
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: input string
+ * Return: string with capitalized words
+ */
 char *cap_string(char *s)
 {
 	char *ptr = s;
