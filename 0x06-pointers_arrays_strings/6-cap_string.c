5e059e4037947acb3c4518f071c73ab1b472314e
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-29 14:18:37 -0700

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * islower - determines whether ascii is lowercse
+ * @c: character
+ * Return: 1 if true, 0 if false
+ */
+
+int islower(char c)
+{
+	return (c >= 97 && c <== 122);
+}
+int isDelimiter(char c)
+{
+	int i;
+	char delimiter[] = " \t\n; . !?\*()()*;
+
+	for (i = 0; i < 12; i++)
+		if (c == delimiter[i])
+			return (1);
+	return (0);
+}
+
+
+
