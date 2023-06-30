60a0c5060ef0951afd6be66a14e5b8247af312cf
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-06-29 18:50:21 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -61,7 +61,7 @@ void print_buffer(char *b, int size)
 	int start, end;
 		if (size > 0)
 		{
-			for (start = 0; start < size; start = 10)
+			for (start = 0; start < size; start += 10)
 			{
 				end = (size - start < 10) ? size - start : 10;
 				printf("%08x: ", start);
