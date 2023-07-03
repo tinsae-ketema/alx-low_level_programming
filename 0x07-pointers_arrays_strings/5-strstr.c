60bd79bd64c6adae0de73ddd9cc4b98a120f065c
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-07-03 13:03:55 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,8 +1,25 @@
 #include "main.h"
 
 /**
- * print_chessboard - print chessboard.
- * @a: rows
- * Return: nothing.
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
  */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
 
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
