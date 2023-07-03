67ff6e74eb6386c254cf5a7d92922e39f56beecf
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-07-03 12:42:34 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,14 +7,14 @@
  */
 void print_chessboard(char (*a)[8])
 {
-        int itr, jtr;
+	int itr, jtr;
 
-        for (itr = 0; itr < 8; itr++)
-        {
-                for (jtr = 0; jtr < 8; jtr++)
-                {
-                        _putchar(a[itr][jtr]);
-                }
-                _putchar('\n');
-        }
+	for (itr = 0; itr < 8; itr++)
+	{
+		for (jtr = 0; jtr < 8; jtr++)
+		{
+			_putchar(a[itr][jtr]);
+		}
+		_putchar('\n');
+	}
 }
