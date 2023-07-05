50e0e388b22872368f72e876f38104f2f8a03e11
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-07-05 06:55:15 -0700

    0x08-recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -21,7 +21,7 @@ char *move_past_star(char *s2)
 int inception(char *s1, char *s2)
 {
 	int ret = 0;
-	
+
 	if (*s1 == 0)
 		return (0);
 	if (*s1 == *s2)
@@ -38,14 +38,15 @@ int inception(char *s1, char *s2)
 int wildcmp(char *s1, char *s2)
 {
 	int ret = 0;
+
 	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
 		return (1);
 	if (*s1 == *s2)
-	{	
+	{
 		if (!*s1)
 			return (1);
 		return (wildcmp(s1 + 1, s2 + 1));
-				}
+	}
 	if (!*s1 || !s2)
 	return (0);
 	if (*s2 == '*')
