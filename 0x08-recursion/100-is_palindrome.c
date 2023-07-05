5da2b01191c78c9d22bd0adc62d969e09affd7e6
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-07-05 05:06:20 -0700

    0x08-recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -41,10 +41,10 @@ int is_palindrome(char *s)
 
 int check(char *s, int start, int end, int mod)
 {
-	if ((start == end && mod != 0 || (start == end + 1 && mod == 0))
+	if ((start == end && mod != 0 || (start == end + 1 && mod == 0)))
 		return (1);
 	else if (s[start] != s [end])
 		return (0);
 	else
-	return (check(s, start + 1, end -1, mod));
+		return (check(s, start + 1, end -1, mod));
 }
