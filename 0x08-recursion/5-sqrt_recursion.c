c510955bc2e2f750bb9de45dfcea8467549cffac
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-07-05 01:53:50 -0700

    0x08. C - Recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - find natural square root
+ * @n: int
+ * @val: square root
+ * Return: int
+ */
+int _square(int n, int val);
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+int square(int n, int val)
+{
+	if (val * val == n)
+		return (val);
+	else if (val * val < n)
+		return (square(n, val + 1));
+	else
+		return (-1);
+}
