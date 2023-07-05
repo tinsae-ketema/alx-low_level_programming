0c23c8306f1359c0ac4320582ea471e1c85f1ae9
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-07-05 00:47:29 -0700

    0x08. C - Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer the string
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	int ntr = 0;
+
+	if (*s > '\0')
+	{
+		ntr += strlen_recursion(s + 1) + 1;
+	}
+	return (ntr);
+}
