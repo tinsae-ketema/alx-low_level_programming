b142e8f3ed57e9e9ceb49f82e6875b90480dfb05
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-07-05 02:31:24 -0700

    0x08. C - Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -23,7 +23,7 @@ int check_prime(int n, int othrn)
 {
 	if (othrn >= n && n > 1)
 		return (1);
-	else if (n  othrn == 0 || n <= 1)
+	else if (n % othrn == 0 || n <= 1)
 		return (0);
 	else 
 		return (check_prime(n, othrn + 1));
