daa02de3fec0f53753852d9860d4da4fbed80b3c
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-07-14 11:37:27 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,10 +1,11 @@
+#ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void _puts(char *str);
-void atoi(const chacr *s);
+int _atoi(const char *s);
 void print_int(unsigned long int n);
 void *malloc_checked(unsigned int b);
 int *array_range(int min, int max);
