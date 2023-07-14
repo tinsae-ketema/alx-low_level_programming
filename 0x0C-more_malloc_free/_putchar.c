9482f6734397f7574dbf2e9863fe47ea6f2c70fb
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-07-14 11:26:44 -0700

    0x0C-more_malloc_free

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - write the character c to stdout
+ * @c: The character to print
+ * Return: On sccess 1.
+ * On error: -1 is returned, and error is set appropriately.
+ */
+int _puchar(char c)
+{
+	|return (write(1, &c, 1));
+}
