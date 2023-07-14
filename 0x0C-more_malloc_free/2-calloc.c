f13b668e81038fe2e2a8a0b5b4adec763cae4ae0
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-07-14 13:37:15 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,10 +14,11 @@ char *_memset(char *s, char b, unsigned int n)
 
 	while (n--)
 		*s++ = b;
+
 	return (ptr);
 }
 /**
- * _calloc - allocates memory for an array,using malloc
+ * *_calloc - allocates memory for an array,using malloc
  * @nmemb: array length
  * @size: size of each element
  * Return: pointer
@@ -32,6 +33,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (m == 0)
 		return (NULL);
+
 	_memset(m, 0, sizeof(int) * nmemb);
 
 	return (m);
