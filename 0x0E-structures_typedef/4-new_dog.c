73a3c4884cab97b952baef6ff6aba818633c7db5
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-07-17 16:47:59 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,13 +7,17 @@
  * @str: the string to get the length
  * Return: length of @str
  */
-int _strlen(const char *str)
+int _strlen(char *s)
 {
-	int length = 0;
+	int i;
 
-	while (*str++)
-		length++;
-	return (length);
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 /**
  * _strcopy - a function that returns @dest with a copy of a string from @src
@@ -23,10 +27,19 @@ int _strlen(const char *str)
  */
 char *_strcopy(char *dest, char *src)
 {
-	int i;
+	int len, i;
 
-	for (i = 0; src[i]; i++)
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++
+	}
+
+	for (i = 0; i < len; i++)
+	{
 		dest[i] = src[i];
+	}
 	dest[i] = '\0';
 
 	return (dest);
@@ -42,30 +55,30 @@ char *_strcopy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	int len1, len2;
 
-	if (!name || age < 0 || !owner)
-		return (NULL);
-
-	dog = (dog_t *) malloc(sizeof(dog_t));
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
-	if ((*dog).name == NULL)
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
 
-	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-	if ((*dog).owner == NULL)
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
 	{
+		free (dog);
 		free(dog->name);
-		free(dog);
 		return (NULL);
 	}
-	dog->name = _strcopy(dog->name, name);
-	dog->age;
-	dog->owner = _strcopy(dog->owner, owner);
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
 
 	return (dog);
 }
