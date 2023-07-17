297dbbbae34eef8b77f4ba1a82b494a4598ef70b
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-07-17 14:33:57 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,9 +12,9 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
-		d->name = "(ni1)";
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		d->owner = "(ni1)";
+		d->owner = "(nil)";
 
 	printf("Name; %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
