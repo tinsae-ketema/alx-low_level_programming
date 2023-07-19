75a3099c6a32c3d79ce7d08ec8a88785c87bbb83
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-07-19 13:54:06 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,20 +8,20 @@
  */
 int main(int argc, char **argv)
 {
-	int (*cp_func)(int, int), a, b;
+	int (*op_func)(int, int), a, b;
 
 	if (argc != 4)
 		printf("Error\n"), exit(98);
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	cp_func = get_op_func(argv[2]);
-	if (!cp_func)
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
 		printf("Error\n"), exit(99);
 
 	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
 		printf("Error\n"), exit(100);
 
-	printf("%d\n", cp_func(a, b));
-	return(0);
+	printf("%d\n", op_func(a, b));
+	return (0);
 }
