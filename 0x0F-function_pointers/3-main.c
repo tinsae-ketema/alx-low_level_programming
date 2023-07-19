04cd555555e27470700448e5cac48547e090c5e0
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-07-19 13:43:32 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	op_func = get_op_func(argv[2]);
+	cp_func = get_op_func(argv[2]);
 	if (!op_func)
 		printf("Error\n"), exit(99);
 
