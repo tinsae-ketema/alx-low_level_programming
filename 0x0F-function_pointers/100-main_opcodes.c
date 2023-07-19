e4749934402928ed5429abb0f1f83e35021ac735
tinsae-ketema <tinsae.ketema@aiesec.net>
2023-07-19 14:11:24 -0700

    0x0F-function_pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the codes for the school students.
+ * @argc: the number of args
+ * @argv: argument vector
+ * Return: Always 0.
+ */
+int main(int argc, chr **argv)
+{
+	char *p = (char *)main;
+	int b;
+
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	b = atoi(argv[1]);
+	if (b < 0)
+		printf("Error\n"), exit(2);
+	while (b--)
+		printf("%02hhx%s", *p++, b ? " " : "\n");
+	return (0);
+}
